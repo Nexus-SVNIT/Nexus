618e1f8c3172a605472a443a76878b5af48db494
Henil Thaker <henilthaker2010@gmail.com>
2023-12-15 19:51:31 +0530

    added feature to fetch list of all events

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -1,13 +1,23 @@
 const Event = require('../models/eventModel.js');
 
+const getAllEvents = async (req, res)=>{
+    try{
+        const allEvents = await Event.find();
+        res.json(allEvents);
+    }
+    catch(err){
+        console.log(err)
+    }
+
+}
 const addEvent = async (req, res)=>{
     const {eventName, eventDate} = req.body;
     try {
         const created_event = await Event.create({eventName, eventDate}); 
         res.json(created_event);
-    } catch (error) {
-        console.log(error);
+    } catch (err) {
+        console.log(err);
     }
 }
 
-module.exports = {addEvent};
+module.exports = {getAllEvents, addEvent};
--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {addEvent} = require('../controllers/eventController.js');
+const {getAllEvents, addEvent} = require('../controllers/eventController.js');
 
 const router = express.Router();
 
+router.get('/', getAllEvents);
 router.post('/create', addEvent);
 
 module.exports = router;
