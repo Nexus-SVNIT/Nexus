5f70121279400d7f52a7732c4c05de4b962dc892
bharat <bhartbhammar3336@gmail.com>
2024-01-14 20:42:43 +0530

    NEw Update ThreeJs updates'

--- client/src/ThreeJs/normalizeVector2.js
+++
@@ -1,5 +0,0 @@
-const normalizeVector2 = function (vector) {
-  vector.x = (vector.x / document.body.clientWidth) * 2 - 1;
-  vector.y = -(vector.y / window.innerHeight) * 2 + 1;
-};
-export default normalizeVector2;
