345d699485173a459f2e4dd154c54aaa6dd126d7
bharat <bhartbhammar3336@gmail.com>
2024-01-15 19:09:37 +0530

    Home Page Scroll Bug Resolved

--- client/src/components/WhatPeopleThinkAboutUs/Testimonial.jsx
+++ client/src/components/WhatPeopleThinkAboutUs/Testimonial.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Testimonial = ({ text, author }) => {
   return (
-    <div className='bg-testimonials object-contain h-[15rem] md:h-[20rem] w-[28rem] md:w-[30rem] flex flex-col items-center justify-center px-10 leading-8 rounded-xl backdrop-blur-md mx-4 md:mx-10 '>
-      <p className=' text-sm md:text-base font-thin text-center line-clamp-5'>{text}</p>
-      <h2 className='mt-2 text-sm md:text-base border-t-4 border-[#3586ff] rounded-sm'>{author}</h2>
+    <div className="bg-testimonials mx-4 flex h-[15rem] w-[28rem] flex-col items-center justify-center rounded-xl object-contain px-10 leading-8 text-white backdrop-blur-md md:mx-10 md:h-[20rem] md:w-[30rem]">
+      <p className=" line-clamp-5 text-center text-sm font-thin md:text-base">
+        {text}
+      </p>
+      <h2 className="mt-2 rounded-sm border-t-4 border-[#3586ff] text-sm md:text-base">
+        {author}
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Testimonial
+export default Testimonial;
--- server/models/memberModel.js
+++ server/models/memberModel.js
@@ -1,44 +1,48 @@
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose')
 
 const MemberInfoSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+  name: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: String,
+    required: true
+  },
+  admNo: {
+    type: String,
+    default: 'U2XCSXXX',
+    required: true
+  },
+  socialLinks: {
+    facebookLink: {
+      type: String,
+      required: true
     },
-    role:{
-        type:String,
-        required:true,
+    linkedinLink: {
+      type: String,
+      required: true
     },
-    email: {
-        type: String,
-        required: true,
-    },
-    image:{
-        type:String,
-        required:true,
-    },
-    year:{
-        type:String,
-        required:true,
-    },
-    socialLinks:{
-        facebookLink:{
-            type:String,
-            required:true,
-        },
-        linkedinLink:{
-            type:String,
-            required:true,
-        },
-        instagramLink:{
-            type:String,
-            required:true,
-        }
+    instagramLink: {
+      type: String,
+      required: true
     }
-    
-});
+  }
 
-const MemberDetailsModel = mongoose.model("MemberDetail", MemberInfoSchema);
+})
 
-module.exports = MemberDetailsModel;
+const MemberDetailsModel = mongoose.model('MemberDetail', MemberInfoSchema)
+
+module.exports = MemberDetailsModel
