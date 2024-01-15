7bbc87c734a88a0867ae5344acca367082b2c3c0
praveen <cbsrirangapraveen.2004@gmail.com>
2024-01-15 22:29:33 +0530

    resolved the margin and padding issue

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -14,11 +14,11 @@ const Home = () => {
     document.body.appendChild(script);
   }, []);
   return (
-    <div className="z-10 mx-auto overflow-x-hidden bg-[#111] transition-all delay-300">
-      <div className="mt-10 flex h-full items-center justify-center md:h-full">
+    <div className=" mx-auto overflow-x-hidden bg-[#111] transition-all delay-300">
+      <div className=" flex flex-col items-center justify-center h-screen">
         <Three />
         <div className="relative z-10 my-8 mt-8 flex w-fit max-w-7xl justify-between md:px-4">
-          <div className="flex w-fit flex-col  items-center justify-center gap-8">
+          <div className="flex w-fit flex-col items-center justify-center gap-8">
             <h1 className="nexus_gradient flex items-center justify-center text-center text-[6rem] md:text-[8rem] ">
               NEXUS
             </h1>
@@ -34,29 +34,10 @@ const Home = () => {
               Explore More
             </Button>
           </div>
-          {/* <div className="relative z-0 hidden pt-6 md:flex lg:flex-1">
-            <div className="bg-screen-gradient my-12 ">
-              <img
-                className="absolute left-[20rem] top-2 h-40 w-80 brightness-75 drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] "
-                src="/assets/learning.png"
-                alt=""
-              />
-              <img
-                className="absolute left-[10rem] top-[12rem] h-40 w-80 brightness-75 drop-shadow-[0_35px_35px_rgba(0,0,0,0.45)]"
-                src="/assets/coding.png"
-                alt=""
-              />
-              <img
-                className="absolute -bottom-8 left-[20rem] h-40 w-80 brightness-75 drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]"
-                src="/assets/play.png"
-                alt=""
-              />
-            </div>
-          </div> */}
         </div>
       </div>
       <label id="terminal" />
-      <div className="mx-auto mt-48 w-full sm:mt-60 md:mt-48">
+      <div className="mx-auto mt-24 w-full sm:mt-60 md:mt-24 z-100">
         <Terminal />
       </div>
       <div className="faq-section relative my-48 w-full ">
