3c8159e86e0eb47f254d48aa4cebd698f2ea3181
praveen <cbsrirangapraveen.2004@gmail.com>
2024-01-18 00:35:47 +0530

    changed the hard coded URL in the forms.jsx

--- client/src/components/Forms/Forms.jsx
+++ client/src/components/Forms/Forms.jsx
@@ -6,66 +6,6 @@ import Error from "../Error/Error";
 import CircularProgress from "@mui/joy/CircularProgress";
 
 const Forms = () => {
-<<<<<<< Updated upstream
-  // const {
-  //   isPending: loading,
-  //   error,
-  //   forms,
-  // } = useQuery({
-  //   queryKey: ["formsData"],
-  //   queryFn: () =>
-  //     fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/member`).then((res) =>
-  //       res.json(),
-  //     ),
-  // });
-  
-  // console.log(forms);
-  // if (error) return <Error />;
-  // if (loading)
-  //   return (
-  //     <div className="flex h-[70vh] w-full items-center justify-center">
-  //       <CircularProgress color="primary" />
-  //     </div>
-  //   );
-  const [forms, setForms] = useState([
-    {
-      _id: 2,
-      title: "Nexus Team Information",
-      type: "General Information",
-      deadline: "2024-12-31",
-      status: "Active",
-      startsFrom: "2024-01-01",
-      participants: 0,
-    },
-    {
-      _id: 1,
-      title: "Fiesta Team",
-      type: "Sport Event",
-      deadline: "2024-12-31",
-      status: "InActive",
-      startsFrom: "2024-01-01",
-      participants: 0,
-    },
-    {
-      _id: 3,
-      title: "WebWonder Team",
-      type: "Web Development",
-      deadline: "2024-12-31",
-      status: "InActive",
-      startsFrom: "2024-01-01",
-      participants: 0,
-    },
-    {
-      _id: 4,
-      title: "CodeSprint",
-      type: "Coding Event",
-      deadline: "Every Sunday",
-      status: "Recurring",
-      startsFrom: "2024-01-01",
-      participants: 0,
-    },
-  ]);
-=======
   const {
     isPending: loading,
     error,
@@ -73,7 +13,7 @@ const Forms = () => {
   } = useQuery({
     queryKey: ["formData"],
     queryFn: () =>
-      fetch(`https://nexus-backend.up.railway.app/forms/`).then((res) =>
+      fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/forms/`).then((res) =>
         res.json(),
       ),
   });
@@ -140,7 +80,6 @@ const Forms = () => {
   //     participants: 0,
   //   },
   // ]);
->>>>>>> Stashed changes
   return (
     <div className="relative mx-auto mb-20 max-w-7xl space-y-8 pb-12">
       <Title>Forms</Title>
