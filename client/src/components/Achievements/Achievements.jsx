560488439d2f6f3d3c3f88fed5b57b6b094d2cb1
bharat <bhartbhammar3336@gmail.com>
2024-01-19 09:00:24 +0530

    New UPdate :Achevement

--- client/src/components/Achievements/Achievements.jsx
+++ client/src/components/Achievements/Achievements.jsx
@@ -2,49 +2,65 @@ import React from "react";
 import Title from "../Title/Title";
 const Array = [
   {
-    timestamp: "1/11/2024 19:23:11",
-    email: "u22cs109@coed.svnit.ac.in",
-    name: "Aasutosh Baraiya _ U22CS109",
-    achievement:
-      "Won 1st Prize in Art and Reel Making Competition - ARTFLIX. In this competition, we had to show some creativity in it. So, we created an art of a dog listening to music on a t-shirt.",
-    imageLink:
-      "https://drive.google.com/open?id=1o8Wr4QDKThfmo25ZnceA_2fPuNhlGJwP",
-    additionalLink:
-      "https://drive.google.com/open?id=1XyInsFEpnCZVq09nZERPS60bH7QADOtO",
-  },
-  {
     timestamp: "1/13/2024 11:46:46",
     email: "u22cs018@coed.svnit.ac.in",
     name: "Suchi Desai_U22CS018, Preesha Sheth_U22CS068, Shambhavi Shinde_U22CS09",
     achievement:
       "First place in the Sports Mania Inter-Year tournament for the sport of Table Tennis",
-    imageLink:
-      "https://drive.google.com/open?id=1MxfOFpwu4w-w43hLRWT3MfkcC-30OZMB",
+    imageLink: "1MxfOFpwu4w-w43hLRWT3MfkcC-30OZMB",
     additionalLink:
       "https://drive.google.com/open?id=1r6O93TXMCE-yVQiJiPrBNl44L3YMSbQ1",
+  },
+  {
+    timestamp: "1/11/2024 19:23:11",
+    email: "u22cs109@coed.svnit.ac.in",
+    name: "Aasutosh Baraiya _ U22CS109",
+    achievement:
+      "Won 1st Prize in Art and Reel Making Competition - ARTFLIX. In this competition, we had to show some creativity in it. So, we created an art of a dog listening to music on a t-shirt.",
+    imageLink: "1o8Wr4QDKThfmo25ZnceA_2fPuNhlGJwP",
+    additionalLink:
+      "https://drive.google.com/open?id=1XyInsFEpnCZVq09nZERPS60bH7QADOtO",
+  },
+  {
+    timestamp: "1/18/2024 22:14:26",
+    email: "u22cs015@coed.svnit.ac.in",
+    name: "Nasir Mansuri - U22CS015",
+    achievement:
+      "I was a part of mindbend design committee previously as well as for now too. It was a great experience working and contributing with them. Waiting for more achievements with NEXUS too!!",
+    imageLink: "1dofh-B78YmVZ5-YfDZYqd1AWlZhBG1Z4",
+    additionalLink: "",
   },
 ];
 const Achievements = () => {
   return (
     <div className="mx-auto mb-48 max-w-7xl">
       <Title>Departmental Achievements</Title>
-      <div className="mt-10 flex flex-wrap items-center justify-center gap-4">
-        {Array.map((el) => (
+      <div className="mt-10 flex flex-wrap items-center justify-center gap-10">
+        {Array.map((el, ind) => (
           <div
             key={el.timestamp}
-            className="flex h-[20rem] w-4/5  rounded-md border "
+            className={`flex w-[90%] flex-col rounded-lg shadow-md shadow-blue-800/75  sm:w-3/4 md:h-4/5 ${
+              ind % 2 ? "md:flex-row-reverse" : "md:flex-row"
+            } md:gap-4 `}
           >
             <div>
               <img
-                src="https://classroomclipart.com/image/static7/preview2/achievement-trophy-surrounded-by-stars-clip-art-59657.jpg"
-                alt=""
-                className="h-[20rem] w-[20rem]"
+                src={
+                  `https://lh3.googleusercontent.com/d/${el.imageLink}` ??
+                  "https://images.pexels.com/photos/1097930/pexels-photo-1097930.jpeg?auto=compress&cs=tinysrgb&w=800"
+                }
+                alt="Banner"
+                className="max-h-60  w-full rounded-t-lg object-cover object-center sm:max-h-[20rem] md:max-h-[18rem] md:w-[30rem]"
               />
             </div>
-            <div className="flex flex-1 flex-col items-center justify-around px-4 py-2">
-              <p>{el.email}</p>
-              <p>{el.name}</p>
-              <p className="w-3/4">{el.achievement}</p>
+
+            <div className="mb-4 mt-4 flex flex-1 flex-col items-center justify-evenly gap-4 overflow-hidden px-4 py-2">
+              <p className="line-clamp-3 w-4/5 text-center font-mono text-sm font-semibold text-green-600 sm:text-base md:line-clamp-none md:text-lg">
+                {el.achievement}
+              </p>
+              <p className="w-4/5 text-center text-xs sm:text-base">
+                {el.name}
+              </p>
             </div>
           </div>
         ))}
--- client/src/components/Connect/Connect.jsx
+++ client/src/components/Connect/Connect.jsx
@@ -7,17 +7,16 @@ import { Link } from "react-router-dom";
 const Connect = () => {
   return (
     <div className="mx-auto mb-20 flex max-w-7xl flex-col items-center justify-center ">
-      <div className="mt-10 flex flex-wrap items-center  gap-3 rounded-md bg-yellow-500/25 p-2 px-4">
-        <FaInfoCircle size={42} />
-        <p className="w-[95%] text-white/75">
+      <div className="mx-4 mt-10 flex items-center gap-3 rounded-md bg-yellow-500/25 p-2 px-4">
+        <FaInfoCircle size={42} className="h-auto" />
+        <p className="w-[95%] text-xs text-white/75 md:text-base">
           Enhance your CSE alumni journey! Join our vibrant community by adding
-          your name to our directory. Click here to join our vibrant community
-          and leave your mark in the legacy!{" "}
+          your name to our directory.
           <Link to="/" className="m-x-4 font-bold  text-white underline">
             {" "}
             Click here
-          </Link>
-          .
+          </Link>{" "}
+          to join our vibrant community and leave your mark in the legacy! .
         </p>
       </div>
       <Title>Alumni Network</Title>{" "}
