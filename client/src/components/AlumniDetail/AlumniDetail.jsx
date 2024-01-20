7b896c22e123c912bf2735fe38e31a8587f0edf7
bharat <bhartbhammar3336@gmail.com>
2024-01-20 12:26:16 +0530

    Social Links added

--- client/src/components/AlumniDetail/AlumniDetail.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import AlumniMenu from "./AlumniMenu";
-
-const AlumniDetail = () => {
-  const [alumniDetails, setAlumniDetails] = useState([]);
-  return <AlumniMenu></AlumniMenu>;
-};
-
-export default AlumniDetail;
--- client/src/data/index.js
+++ client/src/data/index.js
@@ -1,44 +1,39 @@
 export const NavList = [
-  { label: "Home", path: "/" },
-  { label: "Team", path: "/team" },
-  { label: "Achievements", path: "/achievements" },
-  { label: "Events", path: "/events" },
-  { label: "Forms", path: "/forms" },
-  { label: "Connect", path: "/connect" },
-  { label: "About", path: "/about" },
-];
+  { label: 'Home', path: '/' },
+  { label: 'Team', path: '/team' },
+  { label: 'Achievements', path: '/achievements' },
+  { label: 'Events', path: '/events' },
+  { label: 'Forms', path: '/forms' },
+  { label: 'Connect', path: '/connect' },
+  { label: 'About', path: '/about' }
+]
 export const FAQsList = [
   {
     id: 1,
-    question: "What is Club Nexus, and what is its mission?",
-    answer:
-      "Club Nexus is the official club of SVNIT Surat, serving as the departmental club of the Computer Science and Engineering department. It aims to foster collaboration and connection among individuals passionate about technology, innovation, and networking. The club's mission is to create a platform where like-minded individuals can come together, share ideas, and work on projects that contribute to technological advancements.",
+    question: 'What is Club Nexus, and what is its mission?',
+    answer: "Club Nexus is the official club of SVNIT Surat, serving as the departmental club of the Computer Science and Engineering department. It aims to foster collaboration and connection among individuals passionate about technology, innovation, and networking. The club's mission is to create a platform where like-minded individuals can come together, share ideas, and work on projects that contribute to technological advancements."
   },
   {
     id: 2,
-    question: "What types of events and activities does the club organize?",
-    answer:
-      "Club Nexus, the official club of SVNIT Surat's Computer Science and Engineering department, organizes a variety of events and activities. These include workshops, seminars, hackathons, networking sessions, and community outreach programs. The events cover a wide range of topics related to technology, entrepreneurship, and personal development.",
+    question: 'What types of events and activities does the club organize?',
+    answer: "Club Nexus, the official club of SVNIT Surat's Computer Science and Engineering department, organizes a variety of events and activities. These include workshops, seminars, hackathons, networking sessions, and community outreach programs. The events cover a wide range of topics related to technology, entrepreneurship, and personal development."
   },
   {
     id: 3,
-    question: "How can I stay updated about upcoming club events?",
-    answer:
-      "To stay updated about upcoming events organized by Club Nexus, the official club of SVNIT Surat's Computer Science and Engineering department, you can regularly check our official website, follow our social media accounts (Facebook, Twitter, and Instagram), and subscribe to our newsletter. We also recommend joining our online community forums for real-time updates and discussions.",
+    question: 'How can I stay updated about upcoming club events?',
+    answer: "To stay updated about upcoming events organized by Club Nexus, the official club of SVNIT Surat's Computer Science and Engineering department, you can regularly check our official website, follow our social media accounts (Facebook, Twitter, and Instagram), and subscribe to our newsletter. We also recommend joining our online community forums for real-time updates and discussions."
   },
   {
     id: 4,
-    question: "How can I get involved in club projects?",
-    answer:
-      "Getting involved in projects led by Club Nexus, the official club of SVNIT Surat's Computer Science and Engineering department, is easy! Attend our regular meetings and events to network with members and express your interest. You can also check our project boards, which list ongoing and upcoming initiatives. Feel free to reach out to project leads or use our online collaboration platforms to contribute your skills and ideas.",
+    question: 'How can I get involved in club projects?',
+    answer: "Getting involved in projects led by Club Nexus, the official club of SVNIT Surat's Computer Science and Engineering department, is easy! Attend our regular meetings and events to network with members and express your interest. You can also check our project boards, which list ongoing and upcoming initiatives. Feel free to reach out to project leads or use our online collaboration platforms to contribute your skills and ideas."
   },
   {
     id: 5,
-    question: "Are there leadership opportunities within the club?",
-    answer:
-      "Yes, Club Nexus, the official club of SVNIT Surat's Computer Science and Engineering department, encourages leadership development. There are various opportunities for members to take on leadership roles, such as project leads, event coordinators, and executive board members. We regularly accept applications for these positions. Keep an eye on our announcements for leadership opportunities or inquire during club meetings.",
-  },
-];
+    question: 'Are there leadership opportunities within the club?',
+    answer: "Yes, Club Nexus, the official club of SVNIT Surat's Computer Science and Engineering department, encourages leadership development. There are various opportunities for members to take on leadership roles, such as project leads, event coordinators, and executive board members. We regularly accept applications for these positions. Keep an eye on our announcements for leadership opportunities or inquire during club meetings."
+  }
+]
 // https://drive.google.com/file/d/17ikWcuPL64YlKcsTymY6Ubd7kruzwOJp/view?usp=sharing
 export const faculty_advisors = [
   // {
@@ -54,33 +49,33 @@ export const faculty_advisors = [
   // },
 
   {
-    name: "Dr. Bhavesh N. Gohil",
-    role: "Associate Professor",
-    image: "https://www.svnit.ac.in/web/department/computer/images/bng.jpg",
+    name: 'Dr. Bhavesh N. Gohil',
+    role: 'Associate Professor',
+    image: 'https://www.svnit.ac.in/web/department/computer/images/bng.jpg',
     socialLinks: {
-      facebook: "https://www.facebook.com/pranav.dhobi",
-      linkedin: "https://www.linkedin.com/in/pranav-dhobi",
-      instagram: "https://www.instagram.com/pranavdhobi",
-    },
+      googleScholar: 'https://scholar.google.com/citations?user=PyzHYDMAAAAJ&hl=en',
+      googleSite: 'https://sites.google.com/coed.svnit.ac.in/bhavesh-gohil'
+
+    }
   },
   {
-    name: "Dr. Sankita J. Patel",
-    role: "Associate Professor",
-    image: "https://www.svnit.ac.in/sjp/assets/images/propic.png",
+    name: 'Dr. Sankita J. Patel',
+    role: 'Associate Professor',
+    image: 'https://www.svnit.ac.in/sjp/assets/images/propic.png',
     socialLinks: {
-      facebook: "https://www.facebook.com/bharath.bhammar",
-      linkedin: "https://www.linkedin.com/in/bharath-bhammar",
-      instagram: "https://www.instagram.com/bharathbhammar",
-    },
+      facebook: 'https://www.facebook.com/bharath.bhammar',
+      linkedin: 'https://www.linkedin.com/in/bharath-bhammar',
+      instagram: 'https://www.instagram.com/bharathbhammar'
+    }
   },
   {
-    name: "Dr. Dipti P. Rana",
-    role: "Associate Professor",
-    image: "https://www.svnit.ac.in/web/department/computer/images/dpr.jpg",
+    name: 'Dr. Dipti P. Rana',
+    role: 'Associate Professor',
+    image: 'https://www.svnit.ac.in/web/department/computer/images/dpr.jpg',
     socialLinks: {
-      facebook: "https://www.facebook.com/cb.praveen",
-      linkedin: "https://www.linkedin.com/in/cb-praveen",
-      instagram: "https://www.instagram.com/cbpraveen",
-    },
-  },
-];
+      facebook: 'https://www.facebook.com/cb.praveen',
+      linkedin: 'https://www.linkedin.com/in/cb-praveen',
+      instagram: 'https://www.instagram.com/cbpraveen'
+    }
+  }
+]
