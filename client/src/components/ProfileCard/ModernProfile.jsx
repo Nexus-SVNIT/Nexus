9cf16a2f9748216692dd6c729f0d649bb368b81e
Hardik1807 <154869492+Hardik1807@users.noreply.github.com>
2024-08-10 15:55:07 +0530

    Update ModernProfile.jsx

--- client/src/components/ProfileCard/ModernProfile.jsx
+++ client/src/components/ProfileCard/ModernProfile.jsx
@@ -14,14 +14,12 @@ const ModernProfile = ({ profile, isFaculty,img}) => {
       <div className="absolute -left-[5%]  top-0 h-[16rem] w-[20rem] overflow-hidden rounded-b-[50%] border-b-8 border-red-400 bg-red-500/20  md:h-[16rem] md:w-[18rem] ">
         <img
           src={
-            // isFaculty
-            //   ? profile.image
-            //   // : `https://lh3.googleusercontent.com/d/1wuOKZh_DO8tEPCLZfHupGiziwr2BYJGQ`
-              profile.image
-            // img
+            isFaculty
+              ? profile.image
+              : `https://lh3.googleusercontent.com/d/${profile.image}`
           }
           alt="Person"
-          // className="-z-10 flex h-full w-full cursor-pointer object-cover object-top transition-all duration-300 hover:scale-110"
+          className="-z-10 flex h-full w-full cursor-pointer object-cover object-top transition-all duration-300 hover:scale-110"
         />
       </div>
       <div className=" mt-[15.5rem] px-5 py-4 ">
@@ -75,5 +73,4 @@ const ModernProfile = ({ profile, isFaculty,img}) => {
 };
 
 export default ModernProfile;
-
 
