da7d4fdad20d223af52d83449f4db2c8e2e00f62
Hardik1807 <154869492+Hardik1807@users.noreply.github.com>
2024-08-10 15:20:23 +0530

    Update TeamCard.jsx

--- client/src/components/Teams/TeamCard.jsx
+++ client/src/components/Teams/TeamCard.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ModernProfile from "../ProfileCard/ModernProfile";
 
 const TeamCard = ({ data, isFaculty = false }) => {
+  
   return (
     <div className="my-10 flex w-full flex-col items-center justify-center ">
-      <div className="flex w-full flex-wrap items-center justify-center  gap-8 md:gap-10 lg:gap-12 ">
+      <div className="flex w-full flex-wrap items-center justify-center gap-8 md:gap-10 lg:gap-12">
         {data.map((item) => (
           <ModernProfile
             key={item.image}
