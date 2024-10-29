7dce25b99aefd5e726f32cebf20c1ba388c5a095
Devesh Mehta <devesh1217@gmail.com>
2024-10-29 21:48:45 +0530

    Alumni image upload error solved

--- server/routes/alumniRoute.js
+++ server/routes/alumniRoute.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const requireAuth = require('../middlewares/requireAuth.js');
-const multer = require('multer');
 const { allAlumniDetails, addAlumniDetails, allPendingAlumniDetails, allVerifiedAlumniDetails, toggleVerification } = require('../controllers/alumniController.js');
-
 const router = express.Router();
 
-// Configure multer to store file in memory
-const upload = multer({ storage: multer.memoryStorage() });
+const multer = require('multer');
+const path = require('path');
+
+// Configure multer to store files in the writable /tmp directory
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, '/tmp'); // Use /tmp as the temporary directory for serverless environments
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
 
 // Define routes
 router.get('/', allVerifiedAlumniDetails);
