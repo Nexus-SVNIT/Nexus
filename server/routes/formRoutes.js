9232d81bb8da695504a38dfc4ea5b3c11dfbe191
Devesh Mehta <devesh1217@gmail.com>
2025-02-05 17:58:08 +0530

    Bug fix

--- server/routes/formRoutes.js
+++ server/routes/formRoutes.js
@@ -22,7 +22,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 // Routes
 router.get('/', getPublicForms);
 router.get('/all', coreAuthMiddleware, getAllForms);
-router.post('/create', coreAuthMiddleware, createForm);
+// router.post('/create', coreAuthMiddleware, createForm);
 router.post('/submit/:id', authMiddleware, upload.single('file'), submitResponse);
 router.get('/get-responses/:id', coreAuthMiddleware, getResponses);
 router.get('/:id', getFormFields);
@@ -36,17 +36,17 @@ router.patch('/update-deadline/:id', coreAuthMiddleware,updateFormDeadline);
 
 //Route to notify subscribers
 
-router.post('/notify-subscribers/:formId', coreAuthMiddleware, async (req, res) => {
-  const { formId } = req.params;
+// router.post('/notify-subscribers/:formId', coreAuthMiddleware, async (req, res) => {
+//   const { formId } = req.params;
 
-  try {
-      await notifyAllSubscribers(formId);
-      res.status(200).json({ message: 'Subscribers notified successfully.' });
-  } catch (error) {
-      console.error(`Error notifying subscribers: ${error.message}`);
-      res.status(500).json({ message: 'Error notifying subscribers.' });
-  }
-});
+//   try {
+//       await notifyAllSubscribers(formId);
+//       res.status(200).json({ message: 'Subscribers notified successfully.' });
+//   } catch (error) {
+//       console.error(`Error notifying subscribers: ${error.message}`);
+//       res.status(500).json({ message: 'Error notifying subscribers.' });
+//   }
+// });
 
 // router.get('/test/:id', temp);
 
