f6181e1d02d76bcaa6aa91dd5bbf81e1b24975be
Devesh Mehta <devesh1217@gmail.com>
2025-02-21 21:20:17 +0530

    Counter++ added to leaderboard page

--- client/src/components/Form/LeaderBoardPage.jsx
+++ client/src/components/Form/LeaderBoardPage.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import { useParams } from "react-router-dom";
 import LeaderBoard from "./LeaderBoard";
 import { Box } from "@mui/material";
+import incrementCount from "../../libs/increamentCounter";
 
 function LeaderBoardPage() {
   // const { id } = useParams();
+  useEffect(() => {
+    document.title = "LeaderBoard";
+    incrementCount();
+  }, []);
+  
   return (
     <Box sx={{ 
       minHeight: '100vh',
     }}>
-      <title>LeaderBoard</title>
       <LeaderBoard />
     </Box>
   );
