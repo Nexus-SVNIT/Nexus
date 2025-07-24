12cf798e77619848379f5d62103086479f006d8a
Jeet Gupta <guptajeet2506@gmail.com>
2025-07-24 20:39:49 +0530

    Added Collection and Admin route

--- server/routes/issueRoutes.js
+++ server/routes/issueRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {createIssue}=require("../controllers/issueController");
+const {createIssue, getIssues}=require("../controllers/issueController");
+const coreAuthMiddleware = require('../middlewares/coreAuthMiddleware');
 
 // Route for creating an issue
 router.post("/create", createIssue);
+// Admin route for paginated issues
+router.get("/admin", coreAuthMiddleware, getIssues);
 
 module.exports = router;
--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
-const { sendReport } = require('../controllers/reportController');
+const { sendReport, getReports } = require('../controllers/reportController');
+const coreAuthMiddleware = require('../middlewares/coreAuthMiddleware');
 
 // POST /api/report
 router.post('/', upload.single('image'), sendReport);
+// Admin route for paginated reports
+router.get('/admin', coreAuthMiddleware, getReports);
 
 module.exports = router; 
