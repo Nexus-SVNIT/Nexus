fb4185a80073e648dbda6aa420c48d4721096305
Devesh Mehta <devesh1217@gmail.com>
2025-08-13 21:00:12 +0530

    Counter increament only in prod

--- client/src/libs/increamentCounter.js
+++ client/src/libs/increamentCounter.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const incrementCount = async () => {
     try {
-        const response = await axios.post(process.env.REACT_APP_BACKEND_BASE_URL + '/api/counter/increment');
+        if(process.env.REACT_APP_ENV !== 'prod')
+            await axios.post(process.env.REACT_APP_BACKEND_BASE_URL + '/api/counter/increment');
     } catch (error) {
         console.error('Error incrementing count:', error);
     }
