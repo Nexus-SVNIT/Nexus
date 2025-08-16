d9aaff03bd9d9d789469ed5ec324c4c82f3df4a4
Devesh Mehta <devesh1217@gmail.com>
2025-08-16 16:22:39 +0530

    Clean up

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const cloudinary = require('cloudinary').v2
 const mongoose = require('mongoose')
+
 const ExpressError = require('./utils/ExpressError.js')
 const eventRoutes = require('./routes/eventRoutes.js')
 const formRoutes = require('./routes/formRoutes.js')
 const userRoutes = require('./routes/userRoutes.js')
 const achievementRoute = require('./routes/achievementRoute.js')
 const alumniRoute = require('./routes/alumniRoute.js')
-const panelRoutes = require('./routes/panelRoute.js')
 const authRoutes = require('./routes/authRoutes.js')
-const coreRoutes = require('./routes/coreRoutes.js')
 const projectRoutes = require('./routes/projectRoute.js')
 const teamMembersRoutes = require('./routes/teamMembersRoute.js')
 const issueRoutes=require('./routes/issueRoutes.js')
@@ -30,39 +29,32 @@ const app = express()
 const PORT = process.env.PORT
 const MONGO_URL = process.env.MONGO_URL
 
-
-
 app.use(cors());
 
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/health-check', (req, res) => {
     return res.send('EveryThing is Fine.')
 })
 
-cloudinary.config({
-    secure: true
-})
-
 // Rate limiter middleware
-// const limiter = rateLimit({
-//     windowMs: 15 * 60 * 1000, // 15 minutes
-//     max: 50, 
-//     message: 'Too many requests from this IP, please try again after 15 minutes'
-// });
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 50, 
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+});
 
 // Apply the rate limiter to all requests
-// app.use(limiter);
+app.use(limiter);
 
 app.use('/auth', authRoutes)
 app.use('/event', eventRoutes)
 app.use('/forms', formRoutes)
 app.use('/team', teamMembersRoutes)
 app.use('/api/user', userRoutes)
-app.use('/api/core', coreRoutes)
 app.use('/achievements', achievementRoute)
 app.use('/alumni', alumniRoute)
-app.use('/Panel', panelRoutes);
 app.use('/projects', projectRoutes);
 app.use("/api/issue",issueRoutes);
 app.use('/coding-profiles', codingProfileRoutes);
@@ -92,5 +84,4 @@ app.all('*', (req, res, next) => {
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = 'something went wrong' } = err
     res.status(statusCode).send(err.message)
-    // next(err)
 })
