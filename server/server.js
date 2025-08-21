f5ecb860dfacfcdcd907ef8a289a71f62e01fcb6
ASHISH-JHA94 <ajha94023@gmail.com>
2025-08-21 20:17:17 +0530

    readding rate limmiter

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ const limiter = rateLimit({
 });
 
 // Apply the rate limiter to all requests
-// app.use(limiter);
+app.use(limiter);
 
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
