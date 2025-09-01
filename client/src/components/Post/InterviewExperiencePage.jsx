d3fbc4c03213fff9f91c5d8babaf45b6563ddc0b
ASHISH-JHA94 <ajha94023@gmail.com>
2025-09-01 17:07:03 +0530

    Interview Exp Ai Frontend Modified

--- client/src/components/Post/InterviewExperiencePage.jsx
+++ client/src/components/Post/InterviewExperiencePage.jsx
@@ -409,6 +409,7 @@ const InterviewExperiencePage = () => {
         </div>
       )}
      <CompanyAIChatBox companies={companies} defaultCompany={formState.companyFilter} /> 
+     <br/>
 
       {/* Posts Grid */}
       <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
