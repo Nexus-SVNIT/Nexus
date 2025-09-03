a524eaa93d41066af864b2630f2dd31609de1735
Vatsal Bateriwala <vatsalbateriwala562005@gmail.com>
2025-09-03 22:18:52 +0530

    apiservice updated till counter.jsx

--- client/src/services/achievementService.js
+++ client/src/services/achievementService.js
@@ -3,3 +3,7 @@ import API from "./apiService"
 export const getAchivements = async() => {
     return API.get('/achievements/');
 }
+
+export const addAchievement = async (achievementData) => {
+    return API.post('/achievements/add', achievementData);
+}
--- client/src/services/alumniService.js
+++ client/src/services/alumniService.js
@@ -7,3 +7,20 @@ export const getAlumniDetails = async (params) => {
 export const getAllCompaniesAndExpertise = async () => {
     return API.get('/alumni/get-companies-and-expertise');
 };
+
+export const addAlumniDetails = async (alumniData) => {
+    return API.post('/alumni/add', alumniData);
+};
+
+export const postAlumni = async (alumniId, alumniData) => {
+    return API.post(`/auth/alumni/signup/`, alumniData, {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+    );
+};
+
+export const getAllAlumni = async() => {
+    return API.get('/alumni');
+}
