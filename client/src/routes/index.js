dfa3b09ef85d6ef69792fcfe2b5ca931878c4250
Mehul Jain <bingemath08@gmail.com>
2025-11-01 03:07:48 +0530

    final changes

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -13,7 +13,7 @@ import AlumniSignUpForm from '../components/AlumniSignUpForm/AlumniSignUpForm.js
 import Alumni from '../pages/Alumni/Alumni.jsx'
 
 
-import StudyMaterialPage from '../pages/StudyMaterial/SubjectMaterialPage.jsx'
+import SubjectMaterialPage from '../pages/StudyMaterial/SubjectMaterialPage.jsx'
 import SubjectDetailPage from '../pages/StudyMaterial/SubjectDetailPage.jsx'
 
 export const DefaultRoutes = [
